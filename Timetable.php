15a6bd2c0a65f7522563b138ad244f77ff89f0f4
AbhinavChawla8701 <68114391+AbhinavChawla8701@users.noreply.github.com>
2021-08-29 13:51:11 +0530

    Update Timetable.php

--- Timetable.php
+++ Timetable.php
@@ -185,10 +185,7 @@ session_start();
     </table>
 <!--HOME SECTION END-->
 
-<!--<div id="footer">
-    <!--  &copy 2014 yourdomain.com | All Rights Reserved |  <a href="http://binarytheme.com" style="color: #fff" target="_blank">Design by : binarytheme.com</a>
--->
-<!-- FOOTER SECTION END-->
+
 
 <!--  Jquery Core Script -->
 <script src="assets/js/jquery-1.10.2.js"></script>
